4bc25012220d6022b79a6891017159dd352a6472
Arnau Siches <asiches@gmail.com>
2019-09-08 11:17:58 +0100

    chore: reorganise git function
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -7,6 +7,10 @@ grey=$(tput setaf 8)
 
 white=$(tput setaf 15)
 
+git_branch() {
+  git rev-parse --abbrev-ref HEAD 2>/dev/null
+}
+
 update_ps1() {
   export PS1="\[$grey\]\! \[$blue\]\W \[$yellow\]\$(git_branch)\[$grey\]·\[$yellow\]$DOCKER_MACHINE_NAME \[$white\]» \[$reset\]"
 }
