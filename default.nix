e2df1a93fb25f9237db723dc3a071b2e73171e85
Arnau Siches <asiches@gmail.com>
2020-03-09 07:13:58 +0000

    chore: Change nix style to let .. in ..
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- default.nix
+++ default.nix
@@ -1,40 +1,38 @@
-with (import <nixpkgs> {});
-
-{
-  inherit
-    ## Brew
-
-    # bash_5
-    # git
-    # fzf
-    # tmux
-    # opam
-    # vim
-    # openssl
-
-    ## Stable
-
-    direnv
-    dos2unix
-    editorconfig-core-c
-    fd
-    fossil
-    gnused
-    jq
-    mercurial
-    neovim
-    pandoc
-    pass
-    ripgrep
-    tokei
-    xsv
-
-    ## Experimental
-
-    pijul
-    #racket
-    swiProlog
-    tealdeer
-    time
-  ;
-}
+let
+  pkgs = import <nixpkgs> {};
+  # brew = [
+  #   # bash_5
+  #   # git
+  #   # fzf
+  #   # tmux
+  #   # opam
+  #   # vim
+  #   # openssl
+  # ];
+  stable = [
+    pkgs.direnv
+    pkgs.dos2unix
+    pkgs.editorconfig-core-c
+    pkgs.fd
+    pkgs.fossil
+    pkgs.gnused
+    pkgs.jq
+    pkgs.luajit
+    pkgs.mercurial
+    # pkgs.neovim-unwrapped
+    # pkgs.neovim
+    pkgs.pandoc
+    pkgs.pass
+    pkgs.ripgrep
+    pkgs.tokei
+    pkgs.xsv
+  ];
+  experimental = [
+    #pkgs.pijul
+    #pkgs.racket
+    #pkgs.swiProlog
+    pkgs.tealdeer
+    pkgs.time
+  ];
+in
+  stable ++ experimental
