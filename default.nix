7108bbd34d87c8d4e6b3b4282e51384cb7d00469
Arnau Siches <asiches@gmail.com>
2020-03-10 07:30:22 +0000

    chore: Annotate packages
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- default.nix
+++ default.nix
@@ -10,29 +10,56 @@ let
   #   # openssl
   # ];
   stable = [
-    pkgs.direnv
-    pkgs.dos2unix
-    pkgs.editorconfig-core-c
-    pkgs.fd
-    pkgs.fossil
-    pkgs.gnused
-    pkgs.jq
-    pkgs.luajit
-    pkgs.mercurial
-    # pkgs.neovim-unwrapped
     # pkgs.neovim
-    pkgs.pandoc
-    pkgs.pass
-    pkgs.ripgrep
-    pkgs.tokei
-    pkgs.xsv
+    # pkgs.neovim-unwrapped
+    pkgs.bat                 # cat replacement.
+    pkgs.direnv
+    pkgs.dos2unix            # handle line breaks from DOS.
+    pkgs.editorconfig-core-c # configure editor per project.
+    pkgs.exa                 # ls replacement.
+    pkgs.fd                  # find replacement.
+    pkgs.fossil              # sqlite-based VCS.
+    pkgs.git                 # main VCS.
+    pkgs.gnused              # macos sed is ancient.
+    pkgs.jq                  # operate on JSON.
+    pkgs.mercurial           # another VCS.
+    pkgs.pandoc              # transform document formats.
+    pkgs.pass                # manage passwords.
+    pkgs.ripgrep             # grep replacement.
+    pkgs.tealdeer            # man replacement.
+    pkgs.time                # macos time is ancient.
+    pkgs.tokei               # count code.
+    pkgs.xsv                 # operate on CSV, TSV.
   ];
   experimental = [
     #pkgs.pijul
     #pkgs.racket
     #pkgs.swiProlog
-    pkgs.tealdeer
-    pkgs.time
   ];
+
+  # The full list of inputs to install
+  inputs = stable ++ experimental;
 in
-  stable ++ experimental
+  if pkgs.lib.inNixShell
+  then pkgs.mkShell
+    {
+      buildInputs = inputs;
+      shellHook = ''
+        set -o vi
+        local pink='\e[1;35m'
+        local yellow='\e[1;33m'
+        local blue='\e[1;36m'
+        local white='\e[0;37m'
+        local reset='\e[0m'
+
+        git_branch() {
+          git rev-parse --abbrev-ref HEAD 2>/dev/null
+        }
+
+        export PS1="\[$pink\]nix \[$blue\]\W \[$yellow\]\$(git_branch)\[$white\] ∙ \[$reset\]"
+
+        alias ls=exa
+      '';
+    }
+  else
+    inputs
