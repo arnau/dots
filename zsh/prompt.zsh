be4fa480d79fff469dadd0dedd7196f96c407b77
Arnau Siches <asiches@gmail.com>
2020-10-25 10:04:13 +0000

    feat: Add starship for zsh
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,13 +1,2 @@
-setopt PROMPT_SUBST
-autoload -Uz vcs_info
-
-zstyle ':vcs_info:*' actionformats '%F{magenta}%a%f '
-zstyle ':vcs_info:*' formats '%F{yellow}%b '
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
-zstyle ':vcs_info:*' enable git fossil hg
-
-precmd() {
-  vcs_info
-}
-
-PS1='%B%F{blue}%1~%f ${vcs_info_msg_0_}%(?.%F{white}∙.%F{red}%??)%f%b '
+export STARSHIP_CONFIG=~/.dots/starship.toml
+eval "$(starship init zsh)"
