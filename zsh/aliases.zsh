21a22ba792a9db413fe6d051e062ad534d23069d
Arnau Siches <arnau@seachess.net>
2024-08-08 12:06:30 +0100

    chore: Remove eza
    Signed-off-by: Arnau Siches <arnau@seachess.net>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,18 +1,5 @@
 alias vim=nvim
-alias ls=eza
-alias exa=eza
-alias tree='exa -Tl'
 alias scannet='arp -i en0 -a'
-alias serve='devd .'
-# alias serve='python -m SimpleHTTPServer'
-# alias gpg='gpg2'
 alias pasifae='PASSWORD_STORE_DIR=~/pasifae/.pasifae-store pass'
 alias snip='tldr $(tldr --list | tr , "\n" | sk)'
 alias skpid="ps -ef | sed 1d | sk -m --header='[pid:process]' | awk '{print \$2}'"
-
-# requires kitty terminal
-alias icat="kitty +kitten icat"
-
-function lf() {
-  ls "$1" | sk
-}
